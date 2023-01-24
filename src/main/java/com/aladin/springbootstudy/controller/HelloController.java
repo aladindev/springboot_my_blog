844d8efc1fe640c9bd81013cdf961a80ca14a722
aladindev <kjune0205@naver.com>
2023-01-24 16:19:19 +0900

    Spring Boot Study Commit - @PathVariable

--- src/main/java/com/aladin/springbootstudy/controller/HelloController.java
+++ src/main/java/com/aladin/springbootstudy/controller/HelloController.java
@@ -18,6 +18,8 @@ public class HelloController {
     /**
      *  PathVariable : GET 형식의 요청에서 파라미터를 전달하기 위해 URL에 값을 담아
      *  요청하는 방법 {변수}의 이름과 메소드의 매개변수와 일치시켜야 함
+     *  매개변수의 변수명을 일치시킬 수 없는 상황에는
+     * @PathVariable("변수명")으로 선언한다.
      * */
     public String getVariable1(@PathVariable String variable) {
         return variable;
