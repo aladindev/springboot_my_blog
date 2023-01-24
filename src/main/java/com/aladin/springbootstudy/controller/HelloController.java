5c1accaa3a2401a12f38d8a31e2ac834f1fba51c
aladindev <kjune0205@naver.com>
2023-01-24 16:16:58 +0900

    Spring Boot Study Commit - @PathVariable

--- src/main/java/com/aladin/springbootstudy/controller/HelloController.java
+++ src/main/java/com/aladin/springbootstudy/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.aladin.springbootstudy.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +15,11 @@ public class HelloController {
     }
 
     @GetMapping(value="/variable1/{variable}")
+    /**
+     *  PathVariable : GET 형식의 요청에서 파라미터를 전달하기 위해 URL에 값을 담아
+     *  요청하는 방법 {변수}의 이름과 메소드의 매개변수와 일치시켜야 함
+     * */
+    public String getVariable1(@PathVariable String variable) {
+        return variable;
+    }
 }
