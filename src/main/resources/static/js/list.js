de5621f6c1b779b914627adf0b51bed6b0b3eb8c
aladindev <kjune0205@naver.com>
2023-05-01 15:30:42 +0900

    Trade History tab click event

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -82,74 +82,74 @@ window.onload = function(){
         }
     }
 }
-//
-//function eleClick(chkId) {
-//    var eventSource;
-//
-//  switch(chkId) {
-//    case 'tab1' :
-//        var url = "/api/v3/sse/subscribe/today?";
-//        var qStr = "exchngCd=";
-//        for(var i = 0 ; i < document.getElementsByName("exchngCdArr").length ; i++) {
-//                if(i == document.getElementsByName("exchngCdArr").length-1) {
-//                    qStr += document.getElementsByName("exchngCdArr")[i].value;
-//                    break;
-//                }
-//                qStr += document.getElementsByName("exchngCdArr")[i].value;
-//                qStr += "&exchngCd=";
-//        }
-//
-//        eventSource = new EventSource(url + qStr);
-//
-//        $("#thCol2").text("당일시작가");
-//        $("#thCol3").text("당일현재가");
-//        $("#thCol4").text("손익금액");
-//        $("#thCol5").text("%");
-//
-//        eventSource.onopen = (e) => {
-//            console.log("hist sse open");
-//        };
-//
-//        eventSource.onmessage = event => {
-//           var eventJsonArr = JSON.parse(event.data);
-//
-//           for(var i = 0 ; i < eventJsonArr.length ; i++) {
-//              var startAmt = Math.floor(eventJsonArr[i].startAmt);
-//              var nowAmt = Math.floor(eventJsonArr[i].nowAmt);
-//              var diffAmt = Math.floor(eventJsonArr[i].diffAmt);
-//              var percent = (nowAmt - startAmt) / startAmt;
-//              var exchngCd = eventJsonArr[i].exchngCd;
-//              percent = percent.toFixed(2);
-//
-//              $("#" + exchngCd + "startAmt").text(startAmt.toLocaleString() + " 원");
-//              $("#" + exchngCd + "nowAmt").text(nowAmt.toLocaleString() + " 원");
-//              $("#" + exchngCd + "diffAmt").text(diffAmt.toLocaleString() + " 원");
-//              $("#" + exchngCd + "per").text(percent + " %");
-//
-//
-//              if(startAmt > nowAmt) {
-//                  document.getElementById(exchngCd + "nowAmt").style.color = "blue";
-//                  document.getElementById(exchngCd + "diffAmt").style.color = "blue";
-//                  document.getElementById(exchngCd + "per").style.color = "blue";
-//              } else if(Number(bfAmt) < Number(afAmt)) {
-//                  document.getElementById(exchngCd + "nowAmt").style.color = "red";
-//                  document.getElementById(exchngCd + "diffAmt").style.color = "red";
-//                  document.getElementById(exchngCd + "per").style.color = "red";
-//              } else {
-//                  document.getElementById(exchngCd + "nowAmt").style.color = "block";
-//                  document.getElementById(exchngCd + "diffAmt").style.color = "block";
-//                  document.getElementById(exchngCd + "per").style.color = "block";
-//              }
-//           }
-//        };
-//        break;
-//    case 'tab2' :
-//        console.log(chkId);
-//        $("#thCol2").text("전일종료가");
-//        $("#thCol3").text("당일현재가");
-//        $("#thCol4").text("손익금액");
-//        $("#thCol5").text("%");
-//        break;
-//    default : break;
-//  }
-//}
+
+function tabClick(chkId) {
+    var eventSource;
+
+  switch(chkId) {
+    case 'tab1' :
+        var url = "/api/v3/sse/subscribe/today?";
+        var qStr = "exchngCd=";
+        for(var i = 0 ; i < document.getElementsByName("exchngCdArr").length ; i++) {
+                if(i == document.getElementsByName("exchngCdArr").length-1) {
+                    qStr += document.getElementsByName("exchngCdArr")[i].value;
+                    break;
+                }
+                qStr += document.getElementsByName("exchngCdArr")[i].value;
+                qStr += "&exchngCd=";
+        }
+
+        eventSource = new EventSource(url + qStr);
+
+        $("#thCol2").text("당일시작가");
+        $("#thCol3").text("당일현재가");
+        $("#thCol4").text("손익금액");
+        $("#thCol5").text("%");
+
+        eventSource.onopen = (e) => {
+            console.log("hist sse open");
+        };
+
+        eventSource.onmessage = event => {
+           var eventJsonArr = JSON.parse(event.data);
+
+           for(var i = 0 ; i < eventJsonArr.length ; i++) {
+              var startAmt = Math.floor(eventJsonArr[i].startAmt);
+              var nowAmt = Math.floor(eventJsonArr[i].nowAmt);
+              var diffAmt = Math.floor(eventJsonArr[i].diffAmt);
+              var percent = (nowAmt - startAmt) / startAmt;
+              var exchngCd = eventJsonArr[i].exchngCd;
+              percent = percent.toFixed(2);
+
+              $("#" + exchngCd + "startAmt").text(startAmt.toLocaleString() + " 원");
+              $("#" + exchngCd + "nowAmt").text(nowAmt.toLocaleString() + " 원");
+              $("#" + exchngCd + "diffAmt").text(diffAmt.toLocaleString() + " 원");
+              $("#" + exchngCd + "per").text(percent + " %");
+
+
+              if(startAmt > nowAmt) {
+                  document.getElementById(exchngCd + "nowAmt").style.color = "blue";
+                  document.getElementById(exchngCd + "diffAmt").style.color = "blue";
+                  document.getElementById(exchngCd + "per").style.color = "blue";
+              } else if(Number(bfAmt) < Number(afAmt)) {
+                  document.getElementById(exchngCd + "nowAmt").style.color = "red";
+                  document.getElementById(exchngCd + "diffAmt").style.color = "red";
+                  document.getElementById(exchngCd + "per").style.color = "red";
+              } else {
+                  document.getElementById(exchngCd + "nowAmt").style.color = "block";
+                  document.getElementById(exchngCd + "diffAmt").style.color = "block";
+                  document.getElementById(exchngCd + "per").style.color = "block";
+              }
+           }
+        };
+        break;
+    case 'tab2' :
+        console.log(chkId);
+        $("#thCol2").text("전일종료가");
+        $("#thCol3").text("당일현재가");
+        $("#thCol4").text("손익금액");
+        $("#thCol5").text("%");
+        break;
+    default : break;
+  }
+}
