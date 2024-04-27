9f078c60a6d23d0e9bb06c6171721e33cf33229f
aladindev <kjune0205@naver.com>
2024-04-27 16:17:50 +0900

    Bootstrap Clean Blog

--- src/main/java/com/aladin/springbootstudy/controller/MainController.java
+++ src/main/java/com/aladin/springbootstudy/controller/MainController.java
@@ -3,14 +3,26 @@ package com.aladin.springbootstudy.controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+//@RestController
+//@RequestMapping("/api/v1/get-api") // infix = 공통 URL
+//public class MainController {
+//
+//    @GetMapping(value="/login")
+//    public ModelAndView login() {
+//        // 클라우드 변경 Oracle -> AWS
+//        ModelAndView mv = new ModelAndView("login");
+//        return mv;
+//    }
+//}
+
 @RestController
-@RequestMapping("/api/v1/get-api") // infix = 공통 URL
+@RequestMapping("/") // infix = 공통 URL
 public class MainController {
 
-    @GetMapping(value="/login")
+    @GetMapping(value="/index")
     public ModelAndView login() {
         // 클라우드 변경 Oracle -> AWS
         ModelAndView mv = new ModelAndView("login");
-        return mv;   
+        return mv;
     }
 }
