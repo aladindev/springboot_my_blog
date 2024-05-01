feeabb8e480d25478900148408cd1d9353f7ce0c
aladindev <kjune0205@naver.com>
2024-05-01 18:08:58 +0900

    Kakao Login API

--- src/main/java/com/aladin/springbootstudy/service/KakaoService.java
+++ src/main/java/com/aladin/springbootstudy/service/KakaoService.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
@@ -50,5 +51,48 @@ public class KakaoService {
 
 
         return accessToken;
+    }
+
+    public HashMap<String, Object> getUserInfo(String access_Token) throws IOException {
+        // 클라이언트 요청 정보
+        HashMap<String, Object> userInfo = new HashMap<String, Object>();
+
+
+        //------kakao GET 요청------
+        String reqURL = "https://kapi.kakao.com/v2/user/me";
+        URL url = new URL(reqURL);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+
+        int responseCode = conn.getResponseCode();
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        String line = "";
+        String result = "";
+
+        while ((line = br.readLine()) != null) {
+            result += line;
+        }
+
+        logger.info("Response Body : " + result);
+
+        // jackson objectmapper 객체 생성
+        ObjectMapper objectMapper = new ObjectMapper();
+        // JSON String -> Map
+        Map<String, Object> jsonMap = objectMapper.readValue(result, new TypeReference<Map<String, Object>>() {
+        });
+
+
+        //사용자 정보 추출
+        Map<String, Object> properties = (Map<String, Object>) jsonMap.get("properties");
+        Map<String, Object> kakao_account = (Map<String, Object>) jsonMap.get("kakao_account");
+
+        String email = kakao_account.get("email").toString();
+
+        //userInfo에 넣기
+        userInfo.put("email", email);
+
+        return userInfo;
     }
 }
