6746a896475dd3393ad91b3e9cd82711e4b9970d
aladindev <kjune0205@naver.com>
2024-05-01 19:20:15 +0900

    Kakao Login API & 256 Encrypt

---
+++ src/main/java/com/aladin/springbootstudy/dto/DTO_USER_AUTH.java
@@ -0,0 +1,18 @@
+package com.aladin.springbootstudy.dto;
+
+import javax.persistence.Entity;
+
+@Entity
+public class DTO_USER_AUTH {
+    private String user_id;
+    private String auth_cd;
+    private String auth_level;
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+}
---
+++ src/main/java/com/aladin/springbootstudy/dto/DTO_USER_INFO.java
@@ -0,0 +1,18 @@
+package com.aladin.springbootstudy.dto;
+
+import javax.persistence.Entity;
+import java.util.Objects;
+
+@Entity
+public class DTO_USER_INFO {
+    private String user_id;
+    private String secret_key;
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+}
