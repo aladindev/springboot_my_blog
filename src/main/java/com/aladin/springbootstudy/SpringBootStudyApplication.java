8ec3557e3347f42c10a0c0eb2fbf1f35c85b958e
aladindev <kjune0205@naver.com>
2024-05-12 14:05:05 +0900

    Kakao Login API & 256 Encrypt

--- src/main/java/com/aladin/springbootstudy/SpringBootStudyApplication.java
+++ src/main/java/com/aladin/springbootstudy/SpringBootStudyApplication.java
@@ -1,11 +1,13 @@
 package com.aladin.springbootstudy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@MapperScan(basePackages = "com.aladin.springbootstudy.dao")
 public class SpringBootStudyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStudyApplication.class, args);
