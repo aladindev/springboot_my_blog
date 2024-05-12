159c2459339a7118887115b6a75dbbcfad88cf74
aladindev <kjune0205@naver.com>
2024-05-12 13:43:06 +0900

    Kakao Login API & 256 Encrypt

--- src/main/java/com/aladin/springbootstudy/entity/ENTITY_USER_AUTH.java
+++ src/main/java/com/aladin/springbootstudy/entity/ENTITY_USER_AUTH.java
@@ -1,20 +1,20 @@
-package com.aladin.springbootstudy.entity;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import java.util.Objects;
-
-@Entity
-public class ENTITY_USER_AUTH {
-    @Id
-    private String user_id;
-    @Id
-    private String auth_cd;
-    private String auth_level;
-
-    public ENTITY_USER_AUTH(String user_id, String auth_cd, String auth_level) {
-        this.user_id = Objects.requireNonNull(user_id);
-        this.auth_cd = Objects.requireNonNull(auth_cd);
-        this.auth_level = Objects.requireNonNull(auth_level);
-    }
-}
+//package com.aladin.springbootstudy.entity;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import java.util.Objects;
+//
+//@Entity
+//public class ENTITY_USER_AUTH {
+//    @Id
+//    private String user_id;
+//    @Id
+//    private String auth_cd;
+//    private String auth_level;
+//
+//    public ENTITY_USER_AUTH(String user_id, String auth_cd, String auth_level) {
+//        this.user_id = Objects.requireNonNull(user_id);
+//        this.auth_cd = Objects.requireNonNull(auth_cd);
+//        this.auth_level = Objects.requireNonNull(auth_level);
+//    }
+//}
--- src/main/java/com/aladin/springbootstudy/entity/ENTITY_USER_INFO.java
+++ src/main/java/com/aladin/springbootstudy/entity/ENTITY_USER_INFO.java
@@ -1,17 +1,17 @@
-package com.aladin.springbootstudy.entity;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import java.util.Objects;
-
-@Entity
-public class ENTITY_USER_INFO {
-    @Id
-    private String user_id;
-    private String secret_key;
-
-    public ENTITY_USER_INFO(String user_id, String secret_key) {
-        this.user_id = Objects.requireNonNull(user_id);
-        this.secret_key = Objects.requireNonNull(secret_key);
-    }
-}
+//package com.aladin.springbootstudy.entity;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import java.util.Objects;
+//
+//@Entity
+//public class ENTITY_USER_INFO {
+//    @Id
+//    private String user_id;
+//    private String secret_key;
+//
+//    public ENTITY_USER_INFO(String user_id, String secret_key) {
+//        this.user_id = Objects.requireNonNull(user_id);
+//        this.secret_key = Objects.requireNonNull(secret_key);
+//    }
+//}
