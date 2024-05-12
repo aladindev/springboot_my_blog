1471e0d6bf95f4ab870eadd5efed8e3c8b6c7847
aladindev <kjune0205@naver.com>
2024-05-12 21:59:43 +0900

    Configuration properties -> yaml

--- src/main/java/com/aladin/springbootstudy/Configuration.java
+++ src/main/java/com/aladin/springbootstudy/Configuration.java
@@ -1,27 +1,24 @@
 package com.aladin.springbootstudy;
 
 import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 
 @org.springframework.context.annotation.Configuration
 
 public class Configuration {
-    @Bean(name="oauth")
-    public PropertiesFactoryBean kakaoPropertiesFactoryBean() throws Exception {
-        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-        ClassPathResource classPathResource = new ClassPathResource("oauth.yml");
-        propertiesFactoryBean.setLocation(classPathResource);
-
-        return propertiesFactoryBean;
+    @Bean(name = "oauth")
+    public YamlPropertiesFactoryBean kakaoPropertiesFactoryBean() {
+        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+        yaml.setResources(new ClassPathResource("oauth.yml"));
+        return yaml;
     }
 
-    @Bean(name="encrypt")
-    public PropertiesFactoryBean encryptPropertiesFactoryBean() throws Exception {
-        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-        ClassPathResource classPathResource = new ClassPathResource("encrypt.yml");
-        propertiesFactoryBean.setLocation(classPathResource);
-
-        return propertiesFactoryBean;
+    @Bean(name = "encrypt")
+    public YamlPropertiesFactoryBean encryptPropertiesFactoryBean() {
+        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+        yaml.setResources(new ClassPathResource("encrypt.yml"));
+        return yaml;
     }
 }
