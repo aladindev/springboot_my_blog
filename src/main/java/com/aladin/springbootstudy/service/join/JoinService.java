b08fa7d1f49c60fd4129d45f892760c20e77608f
aladindev <kjune0205@naver.com>
2024-05-12 14:16:06 +0900

    Kakao Login API & 256 Encrypt

--- src/main/java/com/aladin/springbootstudy/service/join/JoinService.java
+++ src/main/java/com/aladin/springbootstudy/service/join/JoinService.java
@@ -1,4 +1,7 @@
 package com.aladin.springbootstudy.service.join;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class JoinService {
 }
--- src/main/java/com/aladin/springbootstudy/service/user/UserService.java
+++ src/main/java/com/aladin/springbootstudy/service/user/UserService.java
@@ -1,4 +1,10 @@
 package com.aladin.springbootstudy.service.user;
 
+import com.aladin.springbootstudy.service.oauth.OAuthKakaoService;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private Logger logger = Logger.getLogger(UserService.class);
 }
