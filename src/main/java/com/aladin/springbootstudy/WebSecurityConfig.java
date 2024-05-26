4b4f9745addfb25a4ae6142b75f3789e09b4e7b8
aladindev <kjune0205@naver.com>
2024-05-26 16:37:31 +0900

    session validation and CSRF

---
+++ src/main/java/com/aladin/springbootstudy/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.aladin.springbootstudy;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+          .csrf().and(); // CSRF 보호 활성화 (기본값)
+    }
+}
