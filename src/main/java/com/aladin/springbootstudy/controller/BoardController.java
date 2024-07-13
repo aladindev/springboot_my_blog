c097a7cefc4f69ef1fc721d10b690d9ac517724a
aladindev <kjune0205@naver.com>
2024-07-13 17:32:07 +0900

    test

--- src/main/java/com/aladin/springbootstudy/controller/BoardController.java
+++ src/main/java/com/aladin/springbootstudy/controller/BoardController.java
@@ -34,7 +34,7 @@ public class BoardController {
     }
 
     @GetMapping(value="/category")
-    public ModelAndView category() {  // kafka !!
+    public ModelAndView category() {  // kafka !!e
         logger.warn("post controller ");
         ModelAndView mv = new ModelAndView("board/category");
         return mv;
